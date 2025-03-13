2cba5554f10c02ed526429d43ee6414177ce7526
neekoo499@gmail.com <neekoo499@gmail.com>
2025-03-13 15:12:00 +0530

    First Commit

---
+++ src/main/java/in/psa/TestPsaApplication.java
@@ -0,0 +1,13 @@
+package in.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPsaApplication.class, args);
+	}
+
+}
